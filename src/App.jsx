de9ae22a17c36b6883729753a87004e5128c34c7
Webby Ayush <ayush2210019-d@akgec.ac.in>
2025-04-29 00:04:22 +0530

    component import fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import HowItWorks from './components/HowItWorks'
 import AboutUs from './components/AboutUs'
